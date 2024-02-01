95bdf2efa5edd3f092a311dddf0fcdd856abd8b5
Alex Tran <alextran0899@gmail.com>
2024-02-01 13:05:59 -0600

    add udplistener

--- lasertech-backend/obj/Debug/net8.0/lasertech-backend.AssemblyInfo.cs
+++ lasertech-backend/obj/Debug/net8.0/lasertech-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lasertech-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5408bbe5d2951339985d28f466c056f22907660b")]
 [assembly: System.Reflection.AssemblyProductAttribute("lasertech-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lasertech-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
