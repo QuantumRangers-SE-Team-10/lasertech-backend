6118895b0de69355c4c7fb2a85ee180dd557e9bc
Alex Tran <alextran0899@gmail.com>
2024-02-01 13:07:44 -0600

    add to udplistner to main

--- lasertech-backend/Program.cs
+++ lasertech-backend/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+int udpPort = 7500; // Example port
+
+var udpListenerService = new UdpListenerService(udpPort);
+Task.Run(() => udpListenerService.StartListening()); // Run the UDP listener in a background task
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
