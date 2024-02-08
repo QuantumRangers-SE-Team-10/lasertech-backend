9e40abd4c1b3813efece656b8acd364d53c0a176
Malachi Massey <mm204@uark.edu>
2024-02-08 12:53:21 -0600

    Put the UDP connection in independent async function so that it doesn't block the bootup of the web application. In future may need to consider awaiting a response from this function either to get initalization data, so to await updates.

--- Program.cs
+++ Program.cs
@@ -6,23 +6,35 @@ class Program
 {
     public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
+
+        var builder = WebApplication.CreateBuilder();
 
         var connectionString = builder.Configuration.GetConnectionString("GameContext");
         builder.Services.AddDbContext<GameContext>(options =>
             options.UseNpgsql(connectionString)
             );
 
-        int udpPort = 7500; // Example port
 
-        var udpListenerService = new UdpListenerService(udpPort);
-        Task.Run(() => udpListenerService.StartListening()); // Run the UDP listener in a background task
+        GameConnectionLoop();
 
         var app = builder.Build();
 
         app.MapGet("/", () => "Hello World!");
 
         app.Run();
+
+ 
+
+    }
+
+    private async static void GameConnectionLoop()
+    {
+
+        int udpPort = 7500; // Example port
+
+        var udpListenerService = new UdpListenerService(udpPort);
+        await Task.Run(() => udpListenerService.StartListening()); // Run the UDP listener in a background task
+
     }
 }
 
