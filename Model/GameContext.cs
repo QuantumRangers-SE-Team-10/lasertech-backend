baa34424210475f982f1f58c8abff48dc6f1fb95
Alex Tran <alextran0899@gmail.com>
2024-02-10 21:55:07 -0600

    update players table to include LastUpdate

--- Model/GameContext.cs
+++ Model/GameContext.cs
@@ -4,10 +4,16 @@ namespace lasertech_backend.Model;
 
 public class GameContext: DbContext
 {
-    private DbSet<Player> players { set; get; }
+    public  DbSet<Player> players { set; get; }
 
     public GameContext(DbContextOptions options) : base(options)
     {
         
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        // Configure the schema needed for the game here
+        // For example, setting up table names, relationships, etc.
     }
 }
