ea6928ec76bea520ae0c218793566fefb93d56aa
Alex Tran <alextran0899@gmail.com>
2024-02-10 21:22:49 -0600

    ensure swagger api only show in development environment

--- Program.cs
+++ Program.cs
@@ -1,15 +1,16 @@
 using lasertech_backend.Model;
 using Microsoft.EntityFrameworkCore;
 
-
-
 var builder = WebApplication.CreateBuilder();
 var connectionString = builder.Configuration.GetConnectionString("GameContext");
+var env = builder.Environment;
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<GameContext>(options => options.UseNpgsql(connectionString));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 
 int udpPort = 7500;
 
@@ -17,6 +18,14 @@ var udpListenerService = new UdpListenerService(udpPort);
 
 var app = builder.Build();
 var udp = Task.Run(() => udpListenerService.StartListening());
+
+if (env.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
 
 app.UseHttpsRedirection();
 app.MapControllers();
